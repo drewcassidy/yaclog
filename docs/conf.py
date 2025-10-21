40363b6fc01a4bc4d483228412b659c9e7c95b34
Andrew Cassidy <drewcassidy@me.com>
2025-10-20 22:45:57 -0700

    remove unused css file

--- docs/conf.py
+++ docs/conf.py
@@ -68,7 +68,7 @@ html_favicon = "favicon.ico"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_css_files = ["css/custom.css"]
+html_css_files = []
 
 # -- Options for Autodoc -----------------------------------------------------
 
